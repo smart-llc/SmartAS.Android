69a66098e8a6188fd86f865608e7234a30b22e39
Ivan <ivanmarkov1997@gmail.com>
2018-07-07 00:10:31 +0300

    initial auth activity

---
+++ app/src/main/java/com/example/ivan/smartasreborn/authActivities/AuthActivity.java
@@ -0,0 +1,15 @@
+package com.example.ivan.smartasreborn.authActivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ivan.smartasreborn.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
