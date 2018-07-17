2cea7975ec114a666be5ad3bcb61ad2f3d41507d
Ivan <ivanmarkov1997@gmail.com>
2018-07-17 14:59:47 +0300

    updated design + added logo

--- app/src/main/java/com/example/ivan/smartasreborn/authActivities/AuthActivity.java
+++ app/src/main/java/com/example/ivan/smartasreborn/authActivities/AuthActivity.java
@@ -9,9 +9,10 @@ import android.widget.TextView;
 
 import com.example.ivan.smartasreborn.R;
 
-public class AuthActivity extends AppCompatActivity {
+public class AuthActivity extends AppCompatActivity implements View.OnClickListener{
 
     TextView recoverPassText;
+    TextView registerLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +20,23 @@ public class AuthActivity extends AppCompatActivity {
         setContentView(R.layout.activity_auth);
 
         recoverPassText = (TextView) findViewById(R.id.activity_auth_recover_password_label);
-        recoverPassText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        registerLabel = (TextView) findViewById(R.id.activity_auth_register_label);
+
+        recoverPassText.setOnClickListener(this);
+        registerLabel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.activity_auth_recover_password_label:
                 startActivity(new Intent(getApplicationContext(), RecoverPassActivity.class));
-            }
-        });
+                break;
+            case R.id.activity_auth_register_label:
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+                break;
+            default:
+                break;
+        }
     }
 }
